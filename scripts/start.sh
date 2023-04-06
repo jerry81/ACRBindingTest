f273b21b87e18bd1bf7bddbe9f6e2e40a213ac8a
Raskolnikov <jerry.tann@endress.com>
2023-04-06 12:53:19 +0800

    ✨ prepare testing helm

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,7 @@ aliyun configure set --mode AK --region cn-shanghai --access-key-id $OSS_AK --ac
 
 lastUpdatedTime=$(aliyun oss cat ${OSS_BASE_URL}/lastUpdate.txt | head -1)
 
-backupresult=$(influxd backup -portable -host $INFLUX_HOST -start $lastUpdatedTime tmp | grep "backup complete:
+backupresult=$(influxd backup -portable -host $INFLUX_HOST -start $lastUpdatedTime tmp | grep "backup complete:")
 
 if [ -z "$backupresult" ]
 then
@@ -14,14 +14,12 @@ fi
 
 curDate=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
-aliyun oss mkdir ${OSS_BASE_URL}//$curDate
+aliyun oss mkdir ${OSS_BASE_URL}/$curDate
 
-aliyun oss cp ./tmp ${OSS_BASE_URL}//$curDate -r -f
+aliyun oss cp ./tmp ${OSS_BASE_URL}/$curDate -r -f
 
 touch ./scripts/lastUpdate.txt
 
 $curdate > ./scripts/lastUpdate.txt
 
-aliyun oss cp ./scripts/curDate.txt ${OSS_BASE_URL}
-
-echo "done"
+aliyun oss cp ./scripts/lastUpdate.txt ${OSS_BASE_URL}
