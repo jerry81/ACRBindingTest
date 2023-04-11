3aeb7e7446618e262b544fcea01f7ca9d568d365
Raskolnikov <jerry.tann@endress.com>
2023-04-11 17:27:55 +0800

    ✨ restore example

---
+++ scripts/restore_all_example.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+influx -host $INFLUX_HOST -ssl -username $INFLUX_USERNAME -password $INFLUX_PASSWORD
+
+aliyun configure set --mode AK --region cn-shanghai --access-key-id $OSS_AK --access-key-secret $OSS_AK_SECRET
+
+cd tmp
+
+aliyun oss cp oss://influx-backup-staging -r .
+
+for d in */ ; do
+    influxd restore -host $INFLUX_HOST -db lcm -rp asset_values_retention_policy -portable -newdb tmp $d
+    influx -host $INFLUX_HOST -ssl -username $INFLUX_USERNAME -password $INFLUX_PASSWORD -execute 'SELECT * INTO "lcm".asset_values_retention_policy.:MEASUREMENT FROM "tmp".asset_values_retention_policy./.*/ GROUP BY *'
+    influx -host $INFLUX_HOST -ssl -username $INFLUX_USERNAME -password $INFLUX_PASSWORD -execute 'DROP DATABASE "tmp"'
+done
+
